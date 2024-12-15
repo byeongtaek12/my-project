9ad9af6b02072b9788d7ecacbc86d6895254391d
obt <byeongtaek12@naver.com>
2024-12-15 21:56:23 +0900

    Increased difficulty

---
+++ level 0/Solution6.java
@@ -0,0 +1,49 @@
+class Solution6 {
+    public int solution(int seat, String[][] passengers) {
+        int num_passenger = 0;
+        for(int i=0; i<passengers.length; i++){
+            num_passenger += func4(passengers[i]); //빈칸 부분1 
+            num_passenger -= func3(passengers[i]); //빈칸 부분2 
+        }
+        int answer = func1(seat-num_passenger); //빈칸 부분3 
+        return answer;
+    }
+
+    public int func1(int num){
+        if(0 > num){
+            return 0;
+        }
+        else{
+            return num;
+        }
+    }
+    public int func2(int num){
+        if(num > 0){
+            return 0;
+        }
+        else{
+            return num;
+        }
+    }
+
+    public int func3(String[] station){
+        int num = 0;
+        for(int i=0; i<station.length; i++){
+            if(station[i].equals("Off")){
+                num += 1;
+            }
+        }
+        return num;
+    }
+
+    public int func4(String[] station){
+        int num = 0;
+        for(int i=0; i<station.length; i++){
+            if(station[i].equals("On")){
+                num += 1;
+            }
+        }
+        return num;
+    }
+}
+//빈칸 채우기 빈칸 부분이 있는 곳은 3가지 모두 = 뒤 부터 빈칸 
